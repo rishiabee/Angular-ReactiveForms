73b4c1a26d7c2f5694cc17d2c9e6d725092bd74a
Rishi <42680217+rishiabee@users.noreply.github.com>
2019-10-05 02:41:56 +0400

    Adding dynamic validation rules at run-time.

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -21,6 +21,8 @@ export class CustomerComponent implements OnInit {
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
       email: ['', [Validators.required, Validators.email]],
+      phone: '',
+      notification: 'email',
       sendCatalog: true
     });
   }
@@ -45,4 +47,13 @@ export class CustomerComponent implements OnInit {
     });
   }
 
+  setNotification(type: string) {
+    const phoneControl = this.customerForm.get('phone');
+    if (type === 'text') {
+      phoneControl.setValidators([Validators.required]);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
+  }
 }
